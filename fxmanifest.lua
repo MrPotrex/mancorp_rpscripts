fa4bb88b85ca6523d62672d647c720e9231db666
Mancotrex <32984490+MrPotrex@users.noreply.github.com>
2022-05-12 15:21:31 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean' --'adamant'
+game 'gta5'
+lua54 'yes'
+
+autor 'Mancorp'
+name 'Nombre del script'
+description 'Scripts necesarios al 100% para un servidor de Roleplay | 100% necessary scripts for a Roleplay server '
+version '1.0.0'
+
+server_scripts {}
+
+client_scripts {}
+
+shared_scripts {}
+
+exports {}
+
+-- Si tiene archivos png, css, html, etc... Usarlo, si no, no hace falta.
+files {}
+
+-- Si tiene alguna interfaz usalo, si no, no.
+ui_page {}
+
+-- Si tiene alguna dependencia usalo, si no, no.
+dependency {}
