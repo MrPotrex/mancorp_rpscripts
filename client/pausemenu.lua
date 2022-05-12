c482b02ad311a44dfa0a9e55fb63b3d623555ab9
Mancotrex <32984490+MrPotrex@users.noreply.github.com>
2022-05-12 22:10:59 +0200

    Update pausemenu.lua

--- client/pausemenu.lua
+++ client/pausemenu.lua
@@ -4,6 +4,7 @@ function SetData()
   local name = GetPlayerName(PlayerId())
   local id = GetPlayerServerId(PlayerId())
   Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), 'FE_THDR_GTAO', '' ..Config.servername.. '~w~ | ' ..Config.discord.. '~w~ | ' .._U('id')..'~w~: ' .. id ..  '~w~ | ' .._U('name').. '~w~: '.. name)
+  Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), 'PM_PANE_CFX', Config.servername)
 end
 
 Citizen.CreateThread(function()
