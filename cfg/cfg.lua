e659903fa3c7a600d13ace633492fbf58826a8a3
Mancotrex <32984490+MrPotrex@users.noreply.github.com>
2022-05-12 21:25:52 +0200

    Add files via upload

--- cfg/cfg.lua
+++ cfg/cfg.lua
@@ -1,1 +1,7 @@
 --CONFIGURACIÓN | CONFIG
+Config                  = {}
+Config.Locale          = 'es'
+
+-- PAUSE MENU
+Config.servername = 'SERVER NAME'
+Config.discord = '~b~Discord.gg/CODE' -- CHANGE CODE FOR YOU CODE DISCORD | CAMBIA LO QUE PONDE "CODE" POR EL DE TU DISCORD
